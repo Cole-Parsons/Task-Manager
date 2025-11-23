f748795a80c2dc5d0f9a602c7c206ca49cf7c71f
Surfs-Up5 <cgparsons5@gmail.com>
2025-11-23 16:24:18 -0500

    saves tasks locally to a json file

--- Main.go
+++ Main.go
@@ -1,42 +1,44 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 )
 
-type task struct {
-	name    string
-	dueDate string
+type Task struct {
+	Name    string
+	DueDate string
 	ID      int
 }
 
 func main() {
-	var tasks []task
-	var newTask task
+	var Tasks []Task
+	var NewTask Task
 	var counter int = 0
 	for {
 		i := printChoices()
 		if i == 1 {
 			counter++
-			newTask = createTask(counter)
-			tasks = append(tasks, newTask)
+			NewTask = createTask(counter)
+			Tasks = append(Tasks, NewTask)
 		} else if i == 2 {
-			for j := 0; j < len(tasks); j++ {
-				fmt.Println(tasks[j])
+			for j := 0; j < len(Tasks); j++ {
+				fmt.Println(Tasks[j])
 			}
 		} else if i == 3 {
 			var num int
 			fmt.Println("Which task would you like to remove?")
-			for j := 0; j < len(tasks); j++ {
-				fmt.Println(tasks[j])
+			for j := 0; j < len(Tasks); j++ {
+				fmt.Println(Tasks[j])
 			}
 			fmt.Print("ID Number: ")
 			fmt.Scan(&num)
 			num -= 1
-			tasks = append(tasks[:num], tasks[num+1:]...)
+			Tasks = append(Tasks[:num], Tasks[num+1:]...)
 		} else if i == 4 {
 			fmt.Println("Saving Tasks\n")
+			saveJSON(Tasks)
 		} else if i == 5 {
 			os.Exit(0)
 		} else {
@@ -55,17 +57,32 @@ func printChoices() int {
 	return num
 }
 
-func createTask(num int) task {
+func createTask(num int) Task {
 	var name string
 	var dueDate string
 	fmt.Print("What is the name of the Task?\nEnter Name: ")
 	fmt.Scan(&name)
 	fmt.Print("\nWhen do you want the task to be done?\nEnter Date: ")
 	fmt.Scan(&dueDate)
-	newTask := task{
-		name:    name,
-		dueDate: dueDate,
+	NewTask := Task{
+		Name:    name,
+		DueDate: dueDate,
 		ID:      num,
 	}
-	return newTask
+	return NewTask
+}
+
+func saveJSON(Tasks []Task) {
+	data, err := json.MarshalIndent(Tasks, "", "  ")
+	if err != nil {
+		fmt.Println("Error writing file: ", err)
+		return
+	}
+
+	err = os.WriteFile("tasks.json", data, 0644)
+	if err != nil {
+		fmt.Println("Error writing file: ", err)
+		return
+	}
+	fmt.Println("Tasks saved successfully")
 }
